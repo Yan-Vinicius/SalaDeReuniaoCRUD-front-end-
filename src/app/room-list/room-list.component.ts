c42c9307222e5b80b4d21c977fa2499e00e880fa
Yan-Vinicius <yan.vinicius.queiroz@gmail.com>
2021-09-21 21:25:48 -0300

    first commit

---
+++ src/app/room-list/room-list.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { RoomDetailsComponent } from '../room-details/room-details.component';
+import { Observable } from 'rxjs';
+import { RoomService } from '../room.service';
+import {Room} from "../room";
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-room-list',
+  templateUrl: './room-list.component.html',
+  styleUrls: ['./room-list.component.css']
+})
+export class RoomListComponent implements OnInit {
+
+  rooms!: Observable<Room[]>;
+    
+  constructor(private roomService: RoomService, private router: Router) { }
+
+  ngOnInit() {
+    this.reloadData();
+    this.load();
+
+  }
+
+  reloadData() {
+    this.rooms = this.roomService.getRoomsList();
+    this.load();
+  }
+
+  deleteRoom(id: number) {
+    this.roomService.deleteRoom(id)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.reloadData();
+        },
+        error => console.log(error));
+  }
+
+  roomDetails(id: number){
+    this.router.navigate(['details', id]);
+  }
+
+  updateRoom(id: number){
+    this.router.navigate(['update', id]);
+  }
+  
+  load() {
+    const HAS_RELOAD = 'hasReload';  
+    const hasReload = sessionStorage.getItem(HAS_RELOAD);
+    if (!hasReload) {
+      sessionStorage.setItem(HAS_RELOAD, 'true');
+      location.reload();
+    }
+  }  
+}
